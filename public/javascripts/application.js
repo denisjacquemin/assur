16794688b71dd464846ef4ded5084d71d9746ef7
Denis Jacquemin <denis.jacquemin@gmail.com>
2010-11-29 19:04:47 +0100

    quick search on customers#index page functional

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var quicksearch = null;
+document.observe("dom:loaded", function() {
+  quicksearch = new CustomerFilter('customers');
+  //var filters = new Array();
+  //
+  //listing = new AwesomeListing($$('.awesomelisting .company'), filters);
+
+});
